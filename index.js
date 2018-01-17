3e5c3d5bd7cf797d63115a89d97cf7a1c70f90db
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 18:50:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 const returnFirstTwoDrivers = function(array) { let newarray = []; newarray.push(array[0], array[1]); return newarray;}
 
 const returnLastTwoDrivers = function(array) { let newarray = []; newarray.push(array[array.length - 2], array[array.length - 1]); return newarray;}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
