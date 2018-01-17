b54da4e6973548e9d544aeffddf05d65dca26690
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 18:56:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const returnFirstTwoDrivers = function(array) { let newarray = []; newarray.push
 const returnLastTwoDrivers = function(array) { let newarray = []; newarray.push(array[array.length - 2], array[array.length - 1]); return newarray;}
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier = function
