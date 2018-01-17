b5cadf6cb41e201986290c54bd5de988cb6d4604
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 18:44:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function(array) { let newarray = []; newarray.push(array[0], array[1]); return newarray;}
 
-const returnLastTwoDrivers = function(array) { let newarray = []; newarray.push(array[-2], array[-1]); return newarray;}
+const returnLastTwoDrivers = function(array) { let newarray = []; newarray.push(array[array.length - 2], array[array.length - 1]); return newarray;}
